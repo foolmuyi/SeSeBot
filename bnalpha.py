5b5743d8c8802ad1b17ea4bf76eefc66a7fa66ca
muyi <muyiorlk@gmail.com>
2026-02-04 16:59:23 +0800

    fix news order

--- bnalpha.py
+++ bnalpha.py
@@ -32,6 +32,6 @@ def check_alpha(start_ts):
             news_link = f'https://foresightnews.pro/news/detail/{news_id}'
             if ("alpha" in news_text.lower()) or ("tge" in news_text.lower()):
                 if ("binance" in news_text.lower()) or ("币安" in news_text):
-                    news_msg += f"{news_time}\n{news_title}\n{news_text}\n原文链接：{news_link}\n"
+                    news_msg = f"{news_time}\n{news_title}\n{news_text}\n原文链接：{news_link}\n" + news_msg
     news_res = {'ts': max_ts, 'msg': news_msg}
     return news_res
